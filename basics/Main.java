dc0b320b10d7b53302058dc91031eeb0462c06cd
Yajahira <yari@yvelazquez.com>
2022-08-16 10:54:19 -0400

    completed task 3

--- basics/Main.java
+++ basics/Main.java
@@ -1,3 +1,8 @@
+// import java.time.LocalDate;
+import java.time.LocalDateTime;
+// import java.time.Month;
+import java.time.ZoneId;
+
 public class Main {
   public static void main(String[] args) {
     // System.out.println("test");
@@ -5,6 +10,7 @@ public class Main {
     System.out.println(pluralize(4, "boy"));
     System.out.println(pluralize(0, "me"));
     System.out.println(flipNHeads("1"));
+    System.out.println(clock());
   }
   private static String pluralize(int count, String singular){
     if (count > 0){
@@ -33,4 +39,22 @@ public class Main {
     return flips;
 //Retreived from w3schools.
   }
+  private static LocalDateTime clock(){
+    LocalDateTime now = LocalDateTime.now(ZoneId.of("UTC"));
+    
+    for(int i=1;i!=0;i++){
+      long millis = System.currentTimeMillis();
+      try {
+        Thread.sleep(1000 - millis % 1000);
+      } catch (InterruptedException e) {
+        
+        e.printStackTrace();
+      }
+      //Retreived from stackoverflow.
+      System.out.println(now);
+    }
+    return now;
+  
+  } 
+    
 }
